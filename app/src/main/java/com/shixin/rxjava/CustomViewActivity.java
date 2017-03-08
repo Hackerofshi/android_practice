ead7c1a10fa30f81d22d98a42e9b9be76366c9e9
admin <huamunongzhuang@gmail.com>
2017-03-08 16:38:59 +0800

    readme

--- app/src/main/java/com/shixin/rxjava/CustomViewActivity.java
+++ app/src/main/java/com/shixin/rxjava/CustomViewActivity.java
@@ -2,12 +2,57 @@ package com.shixin.rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import customview.CircleMenuLayout;
 
 public class CustomViewActivity extends AppCompatActivity {
+    private CircleMenuLayout mCircleMenuLayout;
+
+    private String[] mItemTexts = new String[]{"安全中心 ", "特色服务", "投资理财",
+            "转账汇款", "我的账户", "信用卡"};
+    private int[] mItemImgs = new int[]{R.drawable.home_mbank_1_normal,
+            R.drawable.home_mbank_2_normal, R.drawable.home_mbank_3_normal,
+            R.drawable.home_mbank_4_normal, R.drawable.home_mbank_5_normal,
+            R.drawable.home_mbank_6_normal};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //自已切换布局文件看效果
+//		setContentView(R.layout.activity_main02);
         setContentView(R.layout.activity_custom_view);
+
+        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
+
+
+        mCircleMenuLayout.setOnMentItemClickListener(new CircleMenuLayout.OmMenuItemClickListener() {
+            @Override
+            public void itemClick(View view, int pos) {
+                Toast.makeText(CustomViewActivity.this, mItemTexts[pos],
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void itemCenterClick(View view) {
+
+            }
+        });
+
+
+        findViewById(R.id.ll).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/event/MyButton.java
@@ -0,0 +1,67 @@
+package event;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+/**
+ * Created by admin on 2017/3/8 0008.
+ */
+
+public class MyButton extends Button {
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private static final String TAG = MyButton.class.getSimpleName();
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        int action = event.getAction();
+
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                Log.e(TAG, "onTouchEvent ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.e(TAG, "onTouchEvent ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.e(TAG, "onTouchEvent ACTION_UP");
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event)
+    {
+        int action = event.getAction();
+
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                Log.e(TAG, "dispatchTouchEvent ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.e(TAG, "dispatchTouchEvent ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.e(TAG, "dispatchTouchEvent ACTION_UP");
+                break;
+
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+}
