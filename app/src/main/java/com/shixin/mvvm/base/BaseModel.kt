1c13bb987b05366d702615ca01f3ca26f2e91332
shixin <shixin217@126.com>
2020-07-23 15:33:10 +0800

    MVVM

--- app/src/main/java/com/shixin/mvvm/base/BaseModel.kt
+++
@@ -1,8 +0,0 @@
-package com.imyyq.mvvm.base
-
-/**
- * Model 层的基类
- */
-abstract class BaseModel : IModel {
-    override fun onCleared() {}
-}
--- app/src/main/java/com/shixin/mvvm/http/interceptor/logging/Logger.java
+++
@@ -1,5 +0,0 @@
-package com.shixin.mvvm.http.interceptor.logging;
-
-public interface Logger {
-    void log(int level, String tag, String msg);
-}
