7b1c64a9333bedb50c367a63dee20565e12555a1
shixin <shixin217@126.com>
2020-08-03 16:23:21 +0800

    koin 的练习

---
+++ app/src/main/java/com/shixin/koin/ui/MySimpleKoinActivity.kt
@@ -0,0 +1,18 @@
+package com.shixin.koin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shixin.koin.presenter.MySimplePresenter
+import com.shixin.rxjava.R
+import org.koin.android.ext.android.inject
+
+class MySimpleKoinActivity : AppCompatActivity() {
+
+    // Lazy injected MySimplePresenter
+    val firstPresenter: MySimplePresenter by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_simple_koin)
+    }
+}
