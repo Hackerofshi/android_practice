fe4e79cf7f09bdbc25f2a2092db8dd8f256a60ec
shixin <shixin217@126.com>
2020-08-03 18:04:36 +0800

    koin 的练习

--- app/src/main/java/com/shixin/koin/ui/MySimpleKoinActivity.kt
+++ app/src/main/java/com/shixin/koin/ui/MySimpleKoinActivity.kt
@@ -3,6 +3,7 @@ package com.shixin.koin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shixin.koin.presenter.MySimplePresenter
+import com.shixin.koin.reposity.HelloRepositoryImpl
 import com.shixin.rxjava.R
 import org.koin.android.ext.android.inject
 
@@ -10,9 +11,12 @@ class MySimpleKoinActivity : AppCompatActivity() {
 
     // Lazy injected MySimplePresenter
     val firstPresenter: MySimplePresenter by inject()
+    val h: HelloRepositoryImpl by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_simple_koin)
+
+
     }
 }
