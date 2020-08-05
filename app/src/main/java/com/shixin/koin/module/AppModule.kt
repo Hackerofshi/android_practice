f6f2e46bd9df147a6d99f409624059f3fecc4154
shixin <shixin217@126.com>
2020-08-05 11:03:56 +0800

    koin 的练习

--- app/src/main/java/com/shixin/koin/module/AppModule.kt
+++ app/src/main/java/com/shixin/koin/module/AppModule.kt
@@ -1,13 +1,23 @@
 package com.shixin.koin.module
 
+import com.shixin.koin.datasource.RemoteDatasource
 import com.shixin.koin.presenter.MySimplePresenter
 import com.shixin.koin.reposity.HelloRepository
 import com.shixin.koin.reposity.HelloRepositoryImpl
+import com.win.lib_net.net.RetrofitClient
 import org.koin.dsl.module
 
+/**
+ * 声明module
+ */
 object AppModule {
 
     val appModule = module {
+
+
+
+        //datasource实例化，单例
+        single { RetrofitClient.instance }
 
         // single instance of HelloRepository
         single<HelloRepository> { HelloRepositoryImpl() }
---
+++ app/src/main/java/com/shixin/koin/module/RetrofitClient.kt
@@ -0,0 +1,59 @@
+package com.win.lib_net.net
+
+
+
+import com.shixin.rxjava.BuildConfig
+import com.win.lib_net.interceptor.CommonInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+class RetrofitClient private constructor() {
+
+    private var retrofit: Retrofit
+
+    companion object {
+        val instance: RetrofitClient by lazy { RetrofitClient() }
+    }
+
+
+    init {
+
+        retrofit = Retrofit.Builder()
+            .client(initClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://www.wanandroid.com")
+            .build()
+
+    }
+
+    private fun initClient(): OkHttpClient {
+
+        return OkHttpClient.Builder()
+            .addInterceptor(initLogInterceptor())
+            .addInterceptor(CommonInterceptor())
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build()
+    }
+
+    private fun initLogInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+        }
+        return loggingInterceptor
+    }
+
+
+    fun <T> create(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+
+
+}
