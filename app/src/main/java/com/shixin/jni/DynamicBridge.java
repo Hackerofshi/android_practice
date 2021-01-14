f87c9fd55263ba16fec2af544172d4a13d8479c4
shixin <783035017@qq.com>
2021-01-14 11:39:47 +0800

    ndk动态注册

--- app/src/main/java/com/shixin/jni/DynamicBridge.java
+++ app/src/main/java/com/shixin/jni/DynamicBridge.java
@@ -1,5 +1,7 @@
 package com.shixin.jni;
 
+import android.util.Log;
+
 public class DynamicBridge {
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -9,7 +11,17 @@ public class DynamicBridge {
 
     public native int text(String message);
 
-
     public static native int static_text(String message);
 
+    public String name = "ooo";
+
+    public native void native_init();
+
+    public DynamicBridge(){
+        native_init();
+    }
+
+    public void print() {
+        Log.d("mmm", name);
+    }
 }
