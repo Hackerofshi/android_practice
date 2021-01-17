e9d8c72b2c4b7c1a27f910995a46791fcc177798
shixin <783035017@qq.com>
2021-01-17 21:38:06 +0800

    完善

---
+++ app/src/main/java/com/shixin/ui/constraint/ConstraintActivity.kt
@@ -0,0 +1,12 @@
+package com.shixin.ui.constraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shixin.rxjava.R
+
+class ConstraintActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
