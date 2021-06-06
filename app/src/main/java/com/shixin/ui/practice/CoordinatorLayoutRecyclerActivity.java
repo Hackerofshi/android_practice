a31837f9077c7a5ffbbebbb819774c15e4c96abe
shixin <783035017@qq.com>
2021-06-06 20:32:46 +0800

    coordinator 理解

---
+++ app/src/main/java/com/shixin/ui/practice/CoordinatorLayoutRecyclerActivity.java
@@ -0,0 +1,29 @@
+package com.shixin.ui.practice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shixin.rxjava.R;
+
+/**
+ * @ProjectName: Android_Pratice
+ * @Package: com.shixin.ui.practice
+ * @ClassName: CoordinatorLayoutActivity
+ * @Description: java类作用描述
+ * @Author: shixin
+ * @CreateDate: 2021/5/30 15:52
+ * @UpdateUser: shixin：
+ * @UpdateDate: 2021/5/30 15:52
+ * @UpdateRemark: 更新说明：
+ * @Version: 1.0
+ */
+public class CoordinatorLayoutRecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_recycler);
+    }
+}
