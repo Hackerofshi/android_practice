f5a3ebd7fc64470c5e85045c9dfe5feedfa17350
shixin <783035017@qq.com>
2022-01-18 15:36:10 +0800

    apt练习

--- MVVMArchitecture/src/main/java/com/imyyq/mvvm/base/IViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.imyyq.mvvm.base
-
-import androidx.lifecycle.DefaultLifecycleObserver
-
-/**
- * ViewModel 层，让 vm 可以感知 v 的生命周期
- */
-interface IViewModel : DefaultLifecycleObserver
--- MVVMArchitecture/src/main/java/com/imyyq/mvvm/http/interceptor/logging/Logger.java
+++
@@ -1,5 +0,0 @@
-package com.imyyq.mvvm.http.interceptor.logging;
-
-public interface Logger {
-    void log(int level, String tag, String msg);
-}
